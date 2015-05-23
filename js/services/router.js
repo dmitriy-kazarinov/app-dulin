3292519fb25d453cb340293f7fe919baeccc8755
Dima <dima.dulin111@gmail.com>
2015-05-23 16:37:28 +0300

    dev #refactoring #directives

---
+++ js/services/router.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Dima on 23.05.2015.
+ */
+appDulin.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+        $urlRouterProvider.otherwise('/home');
+
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/pages/home.html'
+            })
+            .state('home.directive', {
+                url: '/directive',
+                templateUrl: '/pages/comment-list.html',
+                controller: 'commentCtrl'
+            })
+            .state('home.form', {
+                url: '/form',
+                templateUrl: '/pages/test.html',
+                controller: 'formCtrl'
+            })
+            .state('home.news', {
+                url: '/news',
+                template: '<div class="b-alert alert alert-warning">Мы на вложенной странице news</div>'
+            })
+
+            .state('comment', {
+                url: '/comment',
+                views: {
+                    '': { templateUrl: '/pages/comment.html' },
+                    'columnText@comment': { template: '<div class="well">Страница с разным представлением</div>' },
+                    'columnComment@comment': {
+                        templateUrl: '/pages/comment-table.html',
+                        controller: 'commentCtrl'
+                    }
+                }
+            });
+        //$locationProvider
+        //    .html5Mode({
+        //        enabled: true,
+        //        requireBase: false
+        //    });
+
+    }]);
